380d71709f499a700913faf384837d96d8daac80
pitcha18 <praephenpitcha@gmail.com>
2025-09-12 00:25:01 +0700

    add ex

---
+++ cell03/ex04/ex03bis/todo.js
@@ -0,0 +1,52 @@
+function setcookie(cid, cvalue) {
+    let date = new Date();
+    date.setFullYear(date.getFullYear() + 1);
+    document.cookie = cid + "=" + encodeURIComponent(cvalue) + "; expires=" + date.toUTCString() + "; path=/";
+}
+
+function deletelist(cid) {
+    document.cookie = cid + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+}
+
+function addTask() {
+    let data = prompt("Enter a new task:");
+    let id = Date.now();
+    if (data !== '' && data !== null) {
+        addList(data, id);
+        setcookie(id, data);
+    }
+}
+
+function addList(value, id = 'None') {
+    let div = $("<div></div>")
+        .text(value)
+        .attr("id", id);
+
+    $("#ft_list").prepend(div);
+
+    div.on("click", function () {
+        if (confirm(`Do you really want to delete: "${$(this).text()}" ?`)) {
+            deletelist($(this).attr("id"));
+            $(this).remove();
+        }
+    });
+}
+
+function checkList() {
+    let cookies = document.cookie.split(";");
+    if (cookies.length > 0 && cookies[0] !== "") {
+        for (let i = 0; i < cookies.length; i++) {
+            let part_cookie = cookies[i].split("=");
+            if (part_cookie.length === 2) {
+                let key = part_cookie[0].trim();
+                let value = decodeURIComponent(part_cookie[1]);
+                addList(value, key);
+            }
+        }
+    }
+}
+
+$(document).ready(function () {
+    checkList();
+    $("#newTask").on("click", addTask);
+});
